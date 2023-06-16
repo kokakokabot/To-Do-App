c8461c4dfed8dad5f515a54a9f6048934ac68cde
kokakokabot <kokicd1@gmail.com>
2023-06-16 07:22:15 +0200

    Background work being done

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-todos = []
+import functions
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
 
 while True:
-    user_action = input("Type add or show, or exit: ")
-    user_action = user_action.strip()
+  user_action = input("Type add, show, edit, complete or exit: ")
+  user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input("Enter a todo: ")
-            todos.append(todo)
-        case 'show':
-            for item in todos:
-                print(item)
-        case 'exit':
-            break
+  if user_action.startswith("add"):
+    todo = user_action[4:]
 
+    todos = functions.get_todos()
 
-print('Bye!')
+    todos.append(todo + '\n')
+
+    functions.write_todos(todos)
+
+  elif user_action.startswith("show"):
+    todos = functions.get_todos()
+    for index, item in enumerate(todos):
+      item = item.strip('\n')
